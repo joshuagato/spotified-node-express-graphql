2dc18aa17f0d5d2dfb05bde44858af09a25b124a
Joshua Gato <joshuagato37@gmail.com>
2025-11-29 17:51:57 +0000

    Changed teh database connection

--- util/sequelizedb.js
+++ util/sequelizedb.js
@@ -2,7 +2,7 @@
 const { Sequelize } = require('sequelize');
 /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
 
-// const sequelize = new Sequelize('mysql://root:@localhost:3306/spotified');
+// const sequelize = new Sequelize('mysql://root:password@localhost:3306/spotified');
 // const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
 //   dialect: 'mysql',
 //   host: process.env.HOST_NAME
@@ -20,16 +20,16 @@ const { Sequelize } = require('sequelize');
 //   host: 'localhost'
 // });
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  protocol: 'postgres',
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
-});
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   protocol: 'postgres',
+//   dialect: 'postgres',
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false
+//     }
+//   }
+// });
 
 // const sequelize = new Sequelize('postgres://postgres:ITCstd3712@127.0.0.1:5432/spotified');
 // const sequelize = new Sequelize('postgres://postgres:ITCstd3712@localhost:5432/spotified');
@@ -46,9 +46,9 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 //   dialect: 'postgres',
 // });
 
-// const sequelize = new Sequelize(process.env.DB_NAME2, process.env.DB_USERNAME2, process.env.DB_PASSWORD2, {
-//   port: process.env.PORT2,
-//   host: process.env.HOST_NAME3,
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+//   port: process.env.PORT,
+//   host: process.env.HOST_NAME,
 //   protocol: 'postgres',
 //   dialect: 'postgres',
 //   dialectOptions: {
@@ -62,5 +62,13 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 // const sequelize = new Sequelize("postgres://fvmfyvpzgxsswv:40a3884b431416b21165636c2673f5f3394c3c67fb4316eb1f56e95f85d4e2e9@ec2-107-20-104-234.compute-1.amazonaws.com:5432/d3mbdivi55eov8");
 // const sequelize = new Sequelize("postgres://fvmfyvpzgxsswv:40a3884b431416b21165636c2673f5f3394c3c67fb4316eb1f56e95f85d4e2e9@ec2-107-20-104-234.compute-1.amazonaws.com:5432/d3mbdivi55eov8");
 // const sequelize = new Sequelize("postgres://fvmfyvpzgxsswv:40a3884b431416b21165636c2673f5f3394c3c67fb4316eb1f56e95f85d4e2e9@ec2-107-20-104-234.compute-1.amazonaws.com:5432/d3mbdivi55eov8");
+const sequelize = new Sequelize(process.env.DATABASE_URL3, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
 
 module.exports = sequelize;
