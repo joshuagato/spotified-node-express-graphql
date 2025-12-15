07619c317d3450b08ce15ae384979fa64678bc25
Joshua Gato <joshuagato37@gmail.com>
2025-12-15 11:36:44 +0000

    Added cors handling

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
-require('dotenv').config();
-const fs = require('fs');
-const express = require('express');
-const path = require('path');
-const helmet = require('helmet');
-const compression = require('compression');
-const morgan = require('morgan');
-const cors = require('cors');
+require("dotenv").config();
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const expressStatic = require("express-static-search");
 
-const sequelize = require('./util/sequelizedb');
-const graphqlHttp  = require('express-graphql');
-const graphqlSchema = require('./graphql/schema');
-const graphqlResolver = require('./graphql/resolvers');
-const auth = require('./middleware/auth');
+const sequelize = require("./util/sequelizedb");
+const graphqlHttp = require("express-graphql");
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolver = require("./graphql/resolvers");
+const auth = require("./middleware/auth");
 
 // Initializing express
 const app = express();
 
 // Enable CORS for all Origins
-app.use(cors())
+// app.use(cors())
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true, // allow frontend to send cookies
+    })
+);
 
 // Function for writing our access log to the file system
 const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, 'access.log'),
-  { flags: 'a' }
+    path.join(__dirname, "access.log"),
+    { flags: "a" }
 );
 
 // Initializing the helmet middleware for securing our Request/Response headers
@@ -34,51 +40,63 @@ app.use(helmet());
 app.use(compression());
 
 // Initializing the morgan middleware for Request data logging
-app.use(morgan('combine', { stream: accessLogStream }));
+app.use(morgan("combine", { stream: accessLogStream }));
 
 // Allowing Cross Origin Resource Sharing
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  // res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, PATCH, DELETE"
+    );
+    // res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
 
-  if(req.method === 'OPTIONS') return res.sendStatus(200);
-  next();
+    if (req.method === "OPTIONS") return res.sendStatus(200);
+    next();
 });
 
 // Making the music and artwork folder accessible
-app.use('/music', express.static(path.join(__dirname, 'assets', 'music')));
-app.use('/artwork', express.static(path.join(__dirname, 'assets', 'artwork')));
+app.use("/music", express.static(path.join(__dirname, "assets", "music")));
+app.use("/artwork", express.static(path.join(__dirname, "assets", "artwork")));
 
 // Initializing the auth Middleware
 app.use(auth);
 
 // Middleware for setting up the graphql endpoint
-app.use('/graphql', graphqlHttp({
-  schema: graphqlSchema,
-  rootValue: graphqlResolver,
-  graphiql: true,
-  formatError(err) {
-    if (!err.originalError) return err;
-    
-    const data = err.originalError.data;
-    const message = err.message || 'An error occurred.';
-    const code = err.originalError.code || 500;
-    return {message: message, status: code, data: data};
-  }
-}));
+app.use(
+    "/graphql",
+    graphqlHttp({
+        schema: graphqlSchema,
+        rootValue: graphqlResolver,
+        graphiql: true,
+        formatError(err) {
+            if (!err.originalError) return err;
+
+            const data = err.originalError.data;
+            const message = err.message || "An error occurred.";
+            const code = err.originalError.code || 500;
+            return { message: message, status: code, data: data };
+        },
+    })
+);
 
 // Middleware for handling errors
 app.use((error, req, res, next) => {
-  const status = error.statusCode || 500;
-  const message = error.message;
-  const data = error.data;
-  res.status(status).json({ message: message, data: data });
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
 });
 
 // Connecting the mysql database using sequelize
-sequelize.sync().then(result => {
-  app.listen(4004);
-  console.log('Running on Port: ', 4004);
-}).catch(err => console.log(err));
+sequelize
+    .sync()
+    .then((result) => {
+        app.listen(4004);
+        console.log("Running on Port: ", 4004);
+    })
+    .catch((err) => console.log(err));
